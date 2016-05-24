b7a523e0f7d8f4842525604739eeaed31af6b733
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-05-24 13:12:45 +0900

    fix: flycheck popup error

--- init/22-flycheck.el
+++ init/22-flycheck.el
@@ -1,7 +1,12 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
+#|
 (eval-after-load 'flycheck
   '(custom-set-variables
     '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+|#
+(eval-after-load 'flycheck
+  '(custom-set-variables
+    '(flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)))
 (add-hook 'js2-mode-hook 'flycheck-mode)
 (add-hook 'scala-mode-hook 'flycheck-mode)
 (add-hook 'yaml-mode-hook 'flycheck-mode)
