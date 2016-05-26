fb52e1978a061d2e85d8b8664bf8475eaa4d60b8
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-05-26 14:02:14 +0900

    lazy load slime/yasnippet

--- init/30-slime.el
+++ init/30-slime.el
@@ -1,8 +1,21 @@
 (use-package slime
+  :commands slime
   :ensure t
   :config
   (if (eq system-type 'darwin)
       (setq inferior-lisp-program "/usr/local/bin/sbcl")
     (setq inferior-lisp-program "sbcl"))
   (use-package slime-autoloads)
-  (setq slime-net-coding-system 'utf-8-unix))
+  (setq slime-net-coding-system 'utf-8-unix)
+  (if (locate-library "rosemacs")
+      (slime-setup '(slime-fancy slime-asdf slime-ros slime-repl slime-banner slime-indentation slime-scratch))
+    (slime-setup '(slime-fancy slime-asdf slime-repl slime-banner slime-indentation slime-scratch))))
+
+(use-package ac-slime
+  :ensure t
+  :init
+  (add-hook 'slime-mode-hook 'set-up-slime-ac)
+  (add-hook 'slime-repl-mode-hook 'set-up-slime-ac)
+  :config
+  (eval-after-load "auto-complete"
+    '(add-to-list 'ac-modes 'slime-repl-mode)))
--- init/30-yasnippet.el
+++ init/30-yasnippet.el
@@ -1,5 +1,7 @@
 (use-package yasnippet
   :ensure t
+  :commands (yas-minor-mode yas-expand yas-insert-snippet)
+  :diminish yas-minor-mode
   :config
   (add-to-list 'yas-snippet-dirs (expand-file-name "~/.emacs.d/snippets"))
   (yas-global-mode 1)
