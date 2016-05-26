bf4bc566c21d87dedf374a3955501e1308ebf99a
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-05-26 12:32:49 +0900

    disable marmalade repo

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
 (require 'package)
 ;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
 ;; install minimum 3rdparty packages
