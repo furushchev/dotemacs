cea1363cbf9d6b2928d031c625073b65e2411102
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-10-19 16:47:04 +0200

    add flycheck-pos-tip to init/22-flycheck.el

--- init/22-flycheck.el
+++ init/22-flycheck.el
@@ -6,3 +6,9 @@
     '(custom-set-variables
       '(flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
       '(flycheck-display-errors-delay 0.5))))
+
+(use-package flycheck-pos-tip
+  :ensure flycheck
+  :defer t
+  :config
+  (with-eval-after-load 'flycheck (flycheck-pos-tip-mode)))
