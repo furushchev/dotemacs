e594bc15af6eaf47cf4f3ecbc7e8cf3eca27257d
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-10-19 17:01:34 +0200

    add init/21-git-mode.el

---
+++ init/21-git-mode.el
@@ -0,0 +1,7 @@
+(use-package gitignore-mode
+  :ensure (git-mode)
+  :mode ("gitignore\\'" . gitignore-mode))
+(use-package gitconfig-mode
+  :ensure (git-mode)
+  :mode ("gitconfig\\'" . gitconfig-mode))
+
