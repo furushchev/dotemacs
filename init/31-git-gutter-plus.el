a2e72bc97a356f001e7f7df2f9457272cfa933f6
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-10-21 14:56:56 +0200

    use diff-hl instead of git-gutter+

--- init/31-git-gutter-plus.el
+++ init/31-git-gutter-plus.el
@@ -1,6 +1,6 @@
 (use-package git-gutter+
+  :disabled t
   :load-path "site-lisp/"
-  :ensure t
-  :defer t
+  :load-path "site-lisp/git-modes/"
   :diminish git-gutter+-mode
   :config (global-git-gutter+-mode))
