d46f28df6d9594acddcba63e8d249e17629b0118
Yuki Furuta <me@furushchev.ru>
2019-04-05 18:15:04 +0900

    update copyright

--- templates/50:.*\.py
+++ templates/50:.*\.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Copyright (c) `(format-time-string "%Y")`, GITAI Inc.
+# All rights reserved.
 # Author: `(user-full-name)` <`(user-mail-address)`>
 
 $0
