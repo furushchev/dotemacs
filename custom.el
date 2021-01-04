e47673f9e5efbed231191416ce7a002a3b8fe037
Yuki Furuta <me@furushchev.ru>
2021-01-04 21:58:27 +0900

    update customs

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(lsp-python-ms yasnippet yaml-mode web-mode python-mode lsp-jedi lsp-ivy leaf-convert git-gutter+ flycheck exec-path-from-shell el-get dumb-jump dockerfile-mode cuda-mode counsel company-c-headers cmake-mode ccls blackout arduino-mode)))
+   '(yasnippet yaml-mode web-mode python-mode lsp-python-ms lsp-ivy counsel swiper ivy git-gutter+ flycheck exec-path-from-shell dumb-jump dockerfile-mode cuda-mode cmake-mode company ccls arduino-mode leaf-convert el-get blackout leaf-keywords leaf)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
